8ab8e4b470f73307c34f3ec9ade7c0a255ee1cd4
Timeloop Infosystem <42197429+abhishek08vyas@users.noreply.github.com>
2024-12-18 13:43:34 -0330

    initial commit

---
+++ src/main/java/com/abhishek/bankapp_gateway/BankappGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.bankapp_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankappGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankappGatewayApplication.class, args);
+	}
+
+}
